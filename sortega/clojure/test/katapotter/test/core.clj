120d97bce3670c259f5e0d7e0f84a328f7513f4a
Sebastian Ortega <sortega@tid.es>
2011-11-06 21:53:49 +0100

    Simple discounts

--- sortega/clojure/test/katapotter/test/core.clj
+++ sortega/clojure/test/katapotter/test/core.clj
@@ -14,4 +14,7 @@
   (is (= (* 8 3) (price [2 2 2]))))
 
 (deftest simple-disconunt
-  (is (= (* 8 2 0.95) (price [1 2]))))
+  (is (= (* 8 2 0.95) (price [1 2])))
+  (is (= (* 8 3 0.9) (price [1 3 5])))
+  (is (= (* 8 4 0.8) (price [1 2 3 5])))
+  (is (= (* 8 5 0.75) (price [1 2 3 4 5]))))
