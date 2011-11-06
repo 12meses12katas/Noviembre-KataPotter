467c98172ff65f60fa43f90db0afecab4946863e
Sebastian Ortega <sortega@tid.es>
2011-11-06 19:34:13 +0100

    Clojure version

---
+++ sortega/clojure/project.clj
@@ -0,0 +1,3 @@
+(defproject clojure "1.0.0-SNAPSHOT"
+  :description "KataPotter kata"
+  :dependencies [[org.clojure/clojure "1.2.1"]])
---
+++ sortega/clojure/test/clojure/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure.test.core
+  (:use [clojure.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
