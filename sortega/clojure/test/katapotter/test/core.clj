bee72bf97a82e86dd2c80582d65507be41275425
Sebastian Ortega <sortega@tid.es>
2011-11-06 21:52:32 +0100

    Simple discounts

--- sortega/clojure/test/katapotter/test/core.clj
+++ sortega/clojure/test/katapotter/test/core.clj
@@ -1,6 +1,17 @@
 (ns katapotter.test.core
-  (:use [clojure.core])
+  (:use [katapotter.core])
   (:use [clojure.test]))
 
 (deftest empty-purchase
   (is (= 0 (price []))))
+
+(deftest one-book-purchase
+  (is (= 8 (price [1])))
+  (is (= 8 (price [5]))))
+
+(deftest without-discount
+  (is (= 16 (price [1 1])))
+  (is (= (* 8 3) (price [2 2 2]))))
+
+(deftest simple-disconunt
+  (is (= (* 8 2 0.95) (price [1 2]))))
