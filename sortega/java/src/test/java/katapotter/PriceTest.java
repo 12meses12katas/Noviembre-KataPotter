8cc138ce40be1d1ba8a7dd0484938915c87949bc
Sebastian Ortega <sortega@tid.es>
2011-11-06 16:45:25 +0100

    Red

--- sortega/java/src/test/java/katapotter/PriceTest.java
+++ sortega/java/src/test/java/katapotter/PriceTest.java
@@ -4,15 +4,28 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
+import static katapotter.Price.price;
+import static java.util.Arrays.asList;
 
 public class PriceTest {
 
     @Test()
     public void noBooksNoMoney() {
-        assertEquals(BigDecimal.valueOf(0), Price.price(Arrays.asList()));
+        assertEquals(BigDecimal.valueOf(0), price(Collections.<Integer>emptyList()));
     }
 
+    @Test
+    public void priceWithoutDiscounts() {
+        assertEquals(BigDecimal.valueOf(8), price(asList(0)));
+        assertEquals(BigDecimal.valueOf(8), price(asList(1)));
+        assertEquals(BigDecimal.valueOf(8), price(asList(2)));
+        assertEquals(BigDecimal.valueOf(8), price(asList(3)));
+        assertEquals(BigDecimal.valueOf(8), price(asList(4)));
+        assertEquals(BigDecimal.valueOf(8 * 2), price(asList(0, 0)));
+        assertEquals(BigDecimal.valueOf(8 * 3), price(asList(1, 1, 1)));
+    }
 
 }
