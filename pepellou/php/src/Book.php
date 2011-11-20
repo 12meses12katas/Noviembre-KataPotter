a22629a0c1b98fca2f76288570e62136500de960
Pepe Doval <pepellou@gmail.com>
2011-11-20 13:17:10 +0100

    refactor

--- pepellou/php/src/Book.php
+++ pepellou/php/src/Book.php
@@ -1,28 +1,36 @@
 <?php
 
-define ("FIRST_BOOK",  1);
-define ("SECOND_BOOK", 2);
-define ("THIRD_BOOK",  3);
-define ("FOURTH_BOOK", 4);
-define ("FIFTH_BOOK",  5);
+define ("FIRST_BOOK",  "Harry Potter and the Philosopher's Stone");
+define ("SECOND_BOOK", "Harry Potter and the Chamber of Secrets");
+define ("THIRD_BOOK",  "Harry Potter and the Prisoner of Azkaban");
+define ("FOURTH_BOOK", "Harry Potter and the Goblet of Fire");
+define ("FIFTH_BOOK",  "Harry Potter and the Order of the Phoenix");
 
 class NoSuchBook extends Exception {}
 
 class Book {
 
-	private $number;
+	private static $known_books = array(
+		FIRST_BOOK,
+		SECOND_BOOK,
+		THIRD_BOOK,
+		FOURTH_BOOK,
+		FIFTH_BOOK
+	);
+
+	private $name;
 
 	public function __construct(
-		$book_number
+		$book_name
 	) {
-		$this->number = $book_number;
+		$this->name = $book_name;
 		if (!$this->exists())
 			throw new NoSuchBook;
 	}
 
 	private function exists(
 	) {
-		return ($this->number >= FIRST_BOOK && $this->number <= FIFTH_BOOK);
+		return (in_array($this->name, self::$known_books));
 	}
 
 }
--- pepellou/php/test/BookTest.php
+++ pepellou/php/test/BookTest.php
@@ -6,34 +6,24 @@ class BookTest extends PHPUnit_Framework_TestCase {
 	
 	public function test_there_are_five_different_books(
 	) {
-		$book1 = new Book(1);
-		$book2 = new Book(2);
-		$book3 = new Book(3);
-		$book4 = new Book(4);
-		$book5 = new Book(5);
+		$book1 = new Book(FIRST_BOOK);
+		$book2 = new Book(SECOND_BOOK);
+		$book3 = new Book(THIRD_BOOK);
+		$book4 = new Book(FOURTH_BOOK);
+		$book5 = new Book(FIFTH_BOOK);
 		$this->assertNotEquals($book1, $book2);
 		$this->assertNotEquals($book3, $book4);
 		$this->assertNotEquals($book5, $book2);
-		$this->assertEquals(new Book(2), $book2);
-	}
-
-	public function unexisting_books(
-	) {
-		return array(
-			"negative" => array(-1),
-			"zero" => array(0),
-			"too great" => array(6)
-		);
+		$this->assertEquals(new Book(SECOND_BOOK), $book2);
 	}
 
 	/**
-	 * @dataProvider unexisting_books
 	 * @expectedException NoSuchBook
 	 */
 	public function test_there_are_only_those_books(
-		$book_number
 	) {
-		new Book($book_number);
+		$unexisting_title = "Harry Potter finally dies";
+		new Book($unexisting_title);
 	}
 
 }
