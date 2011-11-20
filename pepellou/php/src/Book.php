950af95e7891a667d36c7dd53ee502dfe6b043aa
Pepe Doval <pepellou@gmail.com>
2011-11-20 13:03:21 +0100

    refactor

--- pepellou/php/src/Book.php
+++ pepellou/php/src/Book.php
@@ -1,5 +1,11 @@
 <?php
 
+define ("FIRST_BOOK",  1);
+define ("SECOND_BOOK", 2);
+define ("THIRD_BOOK",  3);
+define ("FOURTH_BOOK", 4);
+define ("FIFTH_BOOK",  5);
+
 class NoSuchBook extends Exception {}
 
 class Book {
@@ -9,7 +15,7 @@ class Book {
 	public function __construct(
 		$book_number
 	) {
-		if ($book_number <= 0 || $book_number > 5)
+		if ($book_number < FIRST_BOOK || $book_number > FIFTH_BOOK)
 			throw new NoSuchBook;
 		$this->number = $book_number;
 	}
