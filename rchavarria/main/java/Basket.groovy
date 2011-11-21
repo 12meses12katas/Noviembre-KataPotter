42452692f3d6d88d94bcdd472d5414e089649224
Ruben Chavarria <txingo@gmail.com>
2011-11-21 10:35:57 +0100

    Pequeno refactor despues de test en verde

--- rchavarria/main/java/Basket.groovy
+++ rchavarria/main/java/Basket.groovy
@@ -2,16 +2,21 @@
 class Basket {
 
 	def price(def books) {
-		def discount = 0
-		
 		if(!books) return 0
 		
+		def discount = computeDiscount(books)
+		
+		(1 - discount) * 8 * books.size()
+	}
+	
+	private computeDiscount(def books){
+		def discount = 0
 		if(books.size() == 2){
 			if(books[0] != books[1]){
 				discount = 0.05
 			}
 		}
 		
-		(1 - discount) * 8 * books.size()
+		return discount
 	}
 }
