24e85612de6fe081e37ea265fa004a7c68c8762b
Ruben Chavarria <txingo@gmail.com>
2011-11-21 11:26:55 +0100

    Corregida estructura de ficheros a un proyecto maven

--- rchavarria/src/main/java/Basket.groovy
+++ rchavarria/src/main/java/Basket.groovy
@@ -1,7 +1,25 @@
 
 class Basket {
+	private static DISCOUNT_FACTORS = [0, 0, 0.05, 0.1, 0.2, 0.25]
 
+	private BookListUtil bookList = new BookListUtil()
+	
 	def price(def books) {
-		0
+		if(!books) return 0
+		
+		def discount = computeDiscount(books)
+		
+		(1 - discount) * 8 * books.size()
+	}
+	
+	private computeDiscount(def books){
+		def discount = 0
+		
+		if(bookList.areAllDifferent(books)){
+			def discountFactor = books.size()
+			discount = DISCOUNT_FACTORS[discountFactor]
+		}
+		
+		return discount
 	}
 }
--- rchavarria/src/test/java/BasketTest.groovy
+++ rchavarria/src/test/java/BasketTest.groovy
@@ -17,4 +17,59 @@ class BasketTest {
 	public void testNoBooksPrice() {
 		assert 0 == basket.price([])
 	}
+
+	@Test
+	public void testOnlyOneBookPrice() {
+		assert 8 == basket.price([0])
+		assert 8 == basket.price([1])
+		assert 8 == basket.price([2])
+		assert 8 == basket.price([3])
+		assert 8 == basket.price([4])
+	}
+
+	@Test
+	public void testSeveralSameBooksPrice() {
+		assert 16 == basket.price([0,0])
+		assert 16 == basket.price([1,1])
+		assert 24 == basket.price([1,1,1])
+	}
+
+	@Test
+	public void testSimpleFivePercentDiscount() {
+		def discountPrice = 8 * 2 * 0.95
+		
+		assert discountPrice == basket.price([0,1])
+		assert discountPrice == basket.price([1,2])
+		assert discountPrice == basket.price([2,3])
+		assert discountPrice == basket.price([3,4])
+	}
+
+	@Test
+	public void testSimpleTenPercentDiscount() {
+		def discountPrice = 8 * 3 * 0.9
+		
+		assert discountPrice == basket.price([0,1,2])
+		assert discountPrice == basket.price([1,2,3])
+		assert discountPrice == basket.price([2,3,4])
+	}
+
+	@Test
+	public void testSimpleTwentyPercentDiscount() {
+		def discountPrice = 8 * 4 * 0.8
+		
+		assert discountPrice == basket.price([0,1,2,3])
+		assert discountPrice == basket.price([1,2,3,4])
+	}
+
+	@Test
+	public void testSimpleTwentyFivePercentDiscount() {
+		def discountPrice = 8 * 5 * 0.75
+		
+		assert discountPrice == basket.price([0,1,2,3,4])
+	}
+
+	@Test
+	public void testSeveralDiscounts() {
+		assert 8 + (8 * 2 * 0.95) ==  basket.price([0, 0, 1])
+	}
 }
