b5f80987661a03b64639b273d1bbe1344e5631d0
Ruben Chavarria <txingo@gmail.com>
2011-12-01 16:43:46 +0100

    Los tests de IntelligentBookGroup estan en verde

--- rchavarria/src/main/java/IntelligentBookGroup.groovy
+++ rchavarria/src/main/java/IntelligentBookGroup.groovy
@@ -4,6 +4,26 @@ class IntelligentBookGroup {
 	public compute(def books){
 		def groups = [][]
 		
+        def sortedBooks = books.sort()
+        
+        def countZeros = (sortedBooks.findAll { it == 0 }).size()
+
+        (1..countZeros).each {
+            groups[it - 1] = []
+		}
+        
+        int i = 0
+        int j = 0
+        sortedBooks.each {
+            groups[i][j] = it
+            i++
+            
+            if(i == countZeros){
+                i = 0
+                j++
+            }
+        }
+        
 		groups
 	}
 }
--- rchavarria/src/test/java/IntelligentBookGroupTest.groovy
+++ rchavarria/src/test/java/IntelligentBookGroupTest.groovy
@@ -3,7 +3,6 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-
 class IntelligentBookGroupTest {
 
 	private group
