deeec7695d01f4ca3966411fb413991da8919ffd
Juan F. Pérez <j.guitar@gmail.com>
2011-12-19 17:57:48 +0100

    Avanzando en la kata. Añado unas preguntas para contestármelas poco a poco.

--- jguitar/ruby/lib/shopping_basquet.rb
+++ jguitar/ruby/lib/shopping_basquet.rb
@@ -1,5 +1,9 @@
 class Shopping_basquet
   def prize(books)
-    0
+    if books.empty?
+      return 0
+    else
+      return 8
+    end
   end
 end
--- jguitar/ruby/spec/shopping_basquet_spec.rb
+++ jguitar/ruby/spec/shopping_basquet_spec.rb
@@ -1,9 +1,15 @@
 require 'shopping_basquet'
 
-describe "Shopping Basquet Spec" do
-  it "should be a price of 0 with 0 items." do
+describe "Shopping Basquet Basic Specs" do
+  it "should be a price of 0 with 0 items" do
     basquet = Shopping_basquet.new
-    prize = basquet.prize(0)
+    prize = basquet.prize([])
     prize.should == 0
+  end
+  
+  it "should be costs 8 EUR with one copy of any of the five books" do
+    basquet = Shopping_basquet.new
+    prize = basquet.prize([1])
+    prize.should == 8
   end
 end
